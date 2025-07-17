91b08686ca2cb9fb462eb64295f2d4da3204bf76
Ziad Eliwa <ziadeliwa@aucegypt.edu>
2025-07-17 17:41:53 +0300

    Switched to MatLab

--- headers/affine_cipher.h
+++
@@ -1,7 +0,0 @@
-#ifndef AFFINE_CIPHER_H_
-#define AFFINE_CIPHER_H_
-
-void cipher(string input);
-void decypher(string input);
-
-#endif
--- headers/hill_cipher.h
+++
@@ -1,7 +0,0 @@
-#ifndef HILL_CIPHER_H_
-#define HILL_CIPHER_H_
-
-void cipher(string input);
-void decypher(string input);
-
-#endif
